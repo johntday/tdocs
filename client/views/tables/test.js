0f328bf4818471ab4a9bb51b4badeaf0cfe6bb26
johntday <johntday@gmail.com>
2014-02-09 18:55:17 -0600

    table

--- client/views/tables/test.js
+++ client/views/tables/test.js
@@ -2,10 +2,13 @@ Template.test.helpers({
 });
 /*------------------------------------------------------------------------------------------------------------------------------*/
 Template.test.events({
-	'click .maximize': function () {
+	'click #btn-max-table': function (e) {
 		Template.test.maxed = !Template.test.maxed;
+		$(e.currentTarget).html( Template.test.maxed ? 'Minimize' : 'Maximize' );
 		Template.test.table.render();
-		console.log('maxed='+Template.test.maxed);
+	},
+	'click #btn-table-help': function () {
+		alert('show help');
 	}
 
 });
@@ -13,20 +16,22 @@ Template.test.events({
 Template.test.rendered = function() {
 
 	var data = [
-		["", "Maserati", "Mazda", "Mercedes", "Mini", "Mitsubishi"],
+//		["", "Maserati", "Mazda", "Mercedes", "Mini", "Mitsubishi"],
 		["2009", 0, 2941, 4303, 354, 5814],
 		["2010", 5, 2905, 2867, 412, 5284],
 		["2011", 4, 2517, 4822, 552, 6127],
 		["2012", 2, 2422, 5399, 776, 4151]
 	];
 
-	data = createBigData();
+	//data = createBigData();
 
 		var resizeTimeout
-		, availableWidth
-		, availableHeight
-		, $window = $(window)
-		, $example1 = $('#test');
+			, availableWidth
+			, availableHeight
+			, $window = $(window)
+			, $example1 = $('#test')
+			, table_width = 400
+			, table_height = 300;
 
 	var calculateSize = function () {
 		var offset = $example1.offset();
@@ -35,29 +40,67 @@ Template.test.rendered = function() {
 	};
 	$window.on('resize', calculateSize);
 
-	var settings = 			{
+	var settings = {
 		data: data,
 		//colWidths: [55, 47, 47, 47, 47, 47, 47], //can also be a number or a function
-		//fixedRowsTop: 1,
-		rowHeaders: true,
-		colHeaders: true,
+		rowHeaders: false,
+		colHeaders: ["", "Maserati", "Mazda", "Mercedes", "Mini", "Mitsubishi"],
+		fixedRowsTop: 1,
+		columnSorting: true,
+//		currentRowClassName: 'currentRow',
+//		currentColClassName: 'currentCol',
+//		autoWrapRow: true,
 		scrollH: 'auto',
 		scrollV: 'auto',
 		stretchH: 'all',//'hybrid', //default
 		minSpareRows: 1,
-		contextMenu: true,
+		contextMenu: {
+			callback: function (key, options) {
+				if (key === 'change_header') {
+					var header_name = prompt("Enter new header name");
+					var col = options.start._col;
+					settings.colHeaders[col] = header_name;
+					Template.test.table.render();
+				}
+			},
+			items: {
+				"row_above": {},
+				"row_below": {},
+				"hsep1": "---------",
+				"col_left": {},
+				"col_right": {},
+				"hsep2": "---------",
+				"remove_row": {},
+				"remove_col": {},
+				"hsep3": "---------",
+				"undo": {},
+				"redo": {},
+				"change_header": {name: 'Change header name'}
+			}
+		},
+		fillHandle: true,
 		width: function () {
 			if (Template.test.maxed && availableWidth === void 0) {
 				calculateSize();
 			}
-			return Template.test.maxed ? availableWidth : 400;
+			return Template.test.maxed ? availableWidth : table_width;
 		},
 		height: function () {
 			if (Template.test.maxed && availableHeight === void 0) {
 				calculateSize();
 			}
-			return Template.test.maxed ? availableHeight : 300;
+			return Template.test.maxed ? availableHeight : table_height;
 		}
+//		cells: function (row, col, prop) {
+//			var cellProperties = {};
+//			if (row === 0) {
+//				cellProperties.renderer = "firstRowRenderer";
+//				//cellProperties.readOnly = true;
+//			}
+//			else
+//				cellProperties.renderer = "defaultValueRenderer";
+//			return cellProperties;
+//		}
 	};
 
 	if (Template.test.table) {
@@ -66,6 +109,7 @@ Template.test.rendered = function() {
 		Template.test.table = new Handsontable.Core( $example1, settings );
 		Template.test.table.init();
 	}
+
 
 	function createBigData() {
 		var rows = []
