3ffa019a3e774e9fb051705ada2c74abc8cd9778
johntday <johntday@gmail.com>
2014-02-14 10:39:47 -0600

    graph

--- server/fixtures.js
+++ server/fixtures.js
@@ -6,10 +6,6 @@ Meteor.startup(function () {
 	var u = Meteor.users.findOne({username: "admin"}); // find the admin user
 	if(!u)
 		Accounts.createUser({username: "admin", password: "877669", profile: {name: "Stanley Kubrick"}});
-	// Ben Venker
-	u = Meteor.users.findOne({username: "bvenker"}); // find Ben Venker
-	if(!u)
-		Accounts.createUser({username: "bvenker", password: "cocoapuffs", profile: {name: "Ben Venker"}});
 	// John T Day
 	u = Meteor.users.findOne({username: "johntday"}); // find John Day
 	if(!u)
