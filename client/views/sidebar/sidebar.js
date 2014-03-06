9488daba9057c0f7dcda87fbd05dee44d27bd929
johntday <johntday@gmail.com>
2014-03-05 22:14:45 -0600

    tree

--- client/views/sidebar/sidebar.js
+++ client/views/sidebar/sidebar.js
@@ -16,18 +16,43 @@ Template.tmplSidebar.events({
 });
 
 Template.tmpl_bus_layer.rendered = function() {
-	$('#jstree').jstree();
-
-	$('#jstree').on("changed.jstree", function (e, data) {
-		console.log(data.selected);
-	});
-
-	$('button').on('click', function () {
-		$('#jstree').jstree(true).select_node('child_node_1');
-		$('#jstree').jstree('select_node', 'child_node_1');
-		$.jstree.reference('#jstree').select_node('child_node_1');
-	});
-};
+	$('#bus-capabilities').jstree({
+		"core" : {
+			"animation" : 0
+//			"check_callback" : true,
+			,"themes" : { "stripes" : true }
+//			'data' : {
+//				'url' : function (node) {
+//					return node.id === '#' ?
+//					       'ajax_demo_roots.json' : 'ajax_demo_children.json';
+//				},
+//				'data' : function (node) {
+//					return { 'id' : node.id };
+//				}
+//			}
+		},
+//		"types" : {
+//			"#" : {
+//				"max_children" : 1,
+//				"max_depth" : 4,
+//				"valid_children" : ["root"]
+//			},
+//			"root" : {
+//				//"icon" : "/static/3.0.0-beta9/assets/images/tree_icon.png",
+//				"valid_children" : ["default"]
+//			},
+//			"default" : {
+//				"valid_children" : ["default","file"]
+//			},
+//			"file" : {
+//				"icon" : "glyphicon glyphicon-file",
+//				"valid_children" : []
+//			}
+//		},
+		"plugins" : [
+			"contextmenu", "dnd", "search", "state", "types", "wholerow"
+		]
+	});};
 
 Template.tmpl_bus_layer.destroyed = function() {
 	Template['tmpl_bus_layer'].bus_cap = null;
