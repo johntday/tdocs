6de61dfbcda89ead55ea318d310a9f5aa63496c8
johntday <johntday@gmail.com>
2014-03-05 21:24:59 -0600

    tree

--- client/views/sidebar/sidebar.js
+++ client/views/sidebar/sidebar.js
@@ -16,90 +16,17 @@ Template.tmplSidebar.events({
 });
 
 Template.tmpl_bus_layer.rendered = function() {
-	var data = [
-		{
-			label: 'BUSINESS CAPABILITIES',
-			children: [
-				{ label: 'child1' },
-				{ label: 'child2' }
-			]
-		},
-		{
-			label: 'SOMETHING ELSE',
-			children: [
-				{ label: 'child3' }
-			]
-		}
-	];
+	$('#jstree').jstree();
 
-	var $busCap = $('#business-capability');
-	var business_capabilities = Nouns.find({project_id: getProjectId(), class_name: ea.eaType.Business_Capability}).fetch();
-	data = toNodeArray( business_capabilities, "BUSINESS CAPABILITIES" );
+	$('#jstree').on("changed.jstree", function (e, data) {
+		console.log(data.selected);
+	});
 
-//	if (!Template['tmpl_bus_layer'].bus_cap) {
-
-		Template['tmpl_bus_layer'].bus_cap = $busCap.tree({
-			data: data
-			,autoOpen: true
-			,dragAndDrop: true
-			//,saveState: true
-			,slide: true
-			,selectable: true
-			,onCanMove: function(node) {
-				if (! node.parent.parent) {
-					// Example: Cannot move root node
-					return false;
-				} else {
-					return true;
-				}
-			}
-
-		});
-		$busCap.bind(
-			'tree.select',
-			function(event) {
-				if (event.node && event.node.id) {
-					// node was selected
-					var node = event.node;
-					Router.go('/nouns/'+node.id);
-					$busCap.tree('selectNode', null);
-				} else {
-					// event.node is null
-					// a node was deselected
-					// e.previous_node contains the deselected node
-				}
-			}
-		);
-		$busCap.bind(
-			'tree.contextmenu',
-			function(event) {
-				// The clicked node is 'event.node'
-				var node = event.node;
-				//alert(node.name);
-				bootbox.dialog({
-					title: "Context Menu",
-					message: 'What to do with "' + node.name + '"',
-					buttons: {
-						main: {
-							label: "Cancel",
-							className: "btn-default",
-							callback: function() {
-							}
-						},
-						remove: {
-							label: "Delete",
-							className: "btn-danger",
-							callback: function() {
-							}
-						}
-					}
-				});
-			}
-		);
-
-//	} else {
-//		$busCap.tree('loadData', data);
-//	}
+	$('button').on('click', function () {
+		$('#jstree').jstree(true).select_node('child_node_1');
+		$('#jstree').jstree('select_node', 'child_node_1');
+		$.jstree.reference('#jstree').select_node('child_node_1');
+	});
 };
 
 Template.tmpl_bus_layer.destroyed = function() {
