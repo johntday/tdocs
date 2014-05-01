0ffee487bc47a2ce0b1ff455808d09353309aee5
johntday <johntday@gmail.com>
2014-05-01 17:41:09 -0500

    diagrams

--- client/views/nouns/noun_detail.js
+++ client/views/nouns/noun_detail.js
@@ -58,42 +58,6 @@ Template.tmpl_noun_detail.events({
 		if (e.which === 13)
 			$('#btnUpdateNoun').click();
 	},
-	'click #btn_create_rel': function(e) {
-		//e.preventDefault();
-
-		bootbox.dialog({
-			title: "List of Possible Relationships"
-			,message:
-				Template.noun_filter_list_simple({contextVar:'SomeValue'})
-			,buttons: {
-				success: {
-					label: "Select",
-					className: "btn-primary",
-					callback: function() {
-						var $selected = $( "input:checked" );
-						var target_id = $selected.val();
-						var rel_name = $selected.data('relName');
-						createRelationship(target_id, rel_name);
-					}
-				},
-				cancel: {
-					label: "Cancel",
-					className: "btn-default",
-					callback: function() {
-					}
-//				},
-//				showall: {
-//					label: "Show All",
-//					className: "btn-info",
-//					callback: function() {
-//						Router.go('/nounrels');
-//					}
-				}
-			}
-			,onEscape: function() {
-			}
-		});
-	},
 	'click #area': function(e) {
 		e.preventDefault();
 		gotoNounFilterPage('area_code', {value:this.area_code, condition:'$and'});
